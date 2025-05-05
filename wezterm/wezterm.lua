2bae999bf9d83ab9c12920a95b28048b44187c64
Vladyslav Aleksashyn <vladyslav.aleksashyn@einride.tech>
2025-05-05 13:27:54 +0200

    remove wezterm padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,13 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+config.enable_scroll_bar = false
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 config.colors = {
 	background = "#282828",
 	foreground = "#d8d5cc",
